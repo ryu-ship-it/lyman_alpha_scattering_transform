3d9338e22f71806447e254d4f39363b6df9821e1
ryu-ship-it <ruoyu.zhu01@gmail.com>
2026-04-22 19:46:39 +0200

    fixed corrupted file handling

--- loading_data.py
+++ loading_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import glob
 import h5py
+from collections import Counter
 
 class load_data:
     '''
@@ -14,14 +15,26 @@ class load_data:
         spectra_list = glob.glob(f'{spectra_dir}/*z{self.redshift}*.hdf5')
         spectra_data = []
         for file in spectra_list:
-            spectra_file = h5py.File(file, 'r')
-            for key in spectra_file['OpticalDepth'].keys():
-                spectra = np.array(spectra_file[f'OpticalDepth/{key}/Hydrogen_HI/Optical_depths'][:])
-                spectra_data.append(spectra)
-        n_spectra_per_file = len(spectra_file['OpticalDepth'].keys())
-        numof_files = len(spectra_list)
-        numof_spectra = n_spectra_per_file * numof_files
-        optical_depth = np.array(spectra_data).reshape(numof_spectra, -1)
+            try:
+                spectra_file = h5py.File(file, 'r')
+                for key in spectra_file['OpticalDepth'].keys():
+                    spectra = np.array(spectra_file[f'OpticalDepth/{key}/Hydrogen_HI/Optical_depths'][:])
+                    spectra_data.append(spectra)
+                print('File loaded:', file)
+
+            #skip corrupted datafile 
+            except OSError:
+                print('File skipped/corrupted:', file)
+        try:
+            optical_depth = np.array(spectra_data)
+        except ValueError:
+            #skip spectra with different length
+            print('Dataset contains spectra with inconsistent length')
+            lengths = [len(spectra) for spectra in spectra_data]
+            length_of_spectra = Counter(lengths).most_common()[0][0]
+            spectra_data_cleaned = [spectra for spectra in spectra_data if len(spectra) == length_of_spectra]
+            optical_depth = np.array(spectra_data_cleaned)
+
         self.optical_depth = optical_depth
         return self.optical_depth
     
